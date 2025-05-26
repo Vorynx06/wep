77c1647e98a3b8a4c6c620620f7ab960253bebbd
josefeus <josefeus@hotmail.com>
2025-05-26 23:12:32 +0800

    init

---
+++ src/main/java/org/xyz/josefeus/wep/controller/HelloController.java
@@ -0,0 +1,24 @@
+package org.xyz.josefeus.wep.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @Value("${test.user}")
+    private String name;
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/user")
+    public String getUser() {
+        return name;
+    }
+}
---
+++ src/test/java/org/xyz/josefeus/wep/WepApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xyz.josefeus.wep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
